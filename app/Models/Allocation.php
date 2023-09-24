0f9d57a21817042920f5c7ab2212282076579afd
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2023-09-24 23:59:45 +0500

    Update StoreAllocationRequest.php
    added  constraints over here
    they are incomplete  specially merge one

--- app/Models/Allocation.php
+++ app/Models/Allocation.php
@@ -32,8 +32,47 @@ class Allocation extends Model
     {
         return $this->room_id ? true : false;
     }
+    // create a function has Section if section id is not null then return true else false
+    public function hasSection()
+    {
+        return $this->section_id ? true : false;
+    }
+
+    public function hasDay()
+    {
+        return $this->day_id ? true : false;
+    }
+
+    public function hasSlot()
+    {
+        return $this->slot_id ? true : false;
+    }
 
 
+
+    public function getExistingAllocation($fields)
+    {
+        $query = Allocation::query();
+        foreach ($fields as $key => $value) {
+            $query->where($key, $value);
+        }
+        return $query->first();
+    }
+
+
+
+    // create a function doesExists that accepts the array of fields like day_id , slot_id , teacher_id , room_id , course_id , section_id
+    // and check if the allocation is unique or not
+    // the parameter list is dyanmic and where query should be updated accordingly
+
+    public function doesExist($fields)
+    {
+        $query = Allocation::query();
+        foreach ($fields as $key => $value) {
+            $query->where($key, $value);
+        }
+        return $query->exists();
+    }
 
 
     // create a model event creating to check if the allocation is valid or not
@@ -43,22 +82,57 @@ class Allocation extends Model
 
             Log::info('in the creating function of allocation model    ' . $allocation->course);
 
+            $dayid  = $allocation->day->id;
+            $slotid = $allocation->slot->id;
+            $teacherid = $allocation->teacher->id;
+
+
+            $roomid = $allocation->room_id;
+            $courseid = $allocation->course_id;
+            $sectionid = $allocation->section_id;
+
+            
+            // day and slot must be there
+            if (!$allocation->hasDay() || !$allocation->hasSlot()) {
+                throw new Exception('Allocation must have a day and a slot');
+            }
             
             // if hasRoom and doesnot have a teacher or course then it should throw Exception
             if ($allocation->hasRoom() && (!$allocation->hasTeacher() || !$allocation->hasCourse())) {
                 throw new Exception('Room allocation must have a teacher and a course');
             }
-
+            
             // if a Teacher is there then there must be a course
             if ($allocation->hasTeacher() && !$allocation->hasCourse()) {
                 throw new Exception('Teacher allocation must have a course');
             }
-
             
-            
+            // if allocation has a course then section must be there
+            if ($allocation->hasCourse() && !$allocation->hasSection()) {
+                throw new Exception('Course allocation must have a section');
+            }
+            // if a record is there with the same day_id and slot_id and teacher_id with some course id   then even the courseid is changed it should not pass
+            $fields = ['day_id' => $dayid, 'slot_id' => $slotid, 'teacher_id' => $teacherid , 'course_id' => $courseid];
+          
+           
+            if ($allocation->doesExist(['day_id' => $dayid, 'slot_id' => $slotid, 'teacher_id' => $teacherid , 'course_id' => $courseid]) ) {
+                
+                Log::info('Merger :: Allocation Does Exist     ' . $allocation->course);
+                
+               
+                $existingAllocation = $allocation->getExistingAllocation(['day_id' => $dayid, 'slot_id' => $slotid, 'teacher_id' => $teacherid ]);
+           
+                if ($existingAllocation->course->display_code != $allocation->course->display_code) {
+                    throw new Exception('Allocation does exist with different code  ;  Merge was only allowed when course code is same' . json_encode($fields));
+                }
+                if ($existingAllocation->course->id === $allocation->course->id) {
+                    throw new Exception('Allocation does exist: duplicate' . json_encode($fields)   );
+                }
 
-
-
+            }
+            else{
+                Log::info('Merger :: Allocation Does not Exist     ' . $allocation->course);
+            }
         });
     }
 
