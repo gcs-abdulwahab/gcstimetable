0b8fbf718b55eae34b4e00d626495ebd36fd7c35
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2023-10-11 11:51:33 +0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', static function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->string('profile_photo_path', 2048)->nullable();
+
+            $table->rememberToken();
             $table->timestamps();
         });
     }
