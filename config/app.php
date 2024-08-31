94a05c66bcec7574eed95eb80b467357bf688f5d
FaheemRafiq <faheemkhan198742@gmail.com>
2024-08-31 09:28:14 +0500

    telescope

--- config/app.php
+++ config/app.php
@@ -168,7 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Laravel\Telescope\TelescopeServiceProvider::class,
     ])->toArray(),
 
     /*
