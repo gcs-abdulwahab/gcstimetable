f20e044dca185a1b09c75e422f1afc47c2ec9b4f
FaheemRafiq <faheemkhan198742@gmail.com>
2024-09-01 22:33:38 +0500

    pretty scoll

--- resources/js/components/command.tsx
+++ resources/js/components/command.tsx
@@ -70,7 +70,7 @@ export function CommandDialogDemo() {
             >
                 <p className="text-sm text-muted-foreground">
                     Press{" "}
-                    <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100">
+                    <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 px-1.5 font-mono text-[10px] font-medium opacity-100">
                         <span className="text-xs">{superKey}</span>k
                     </kbd>
                 </p>
